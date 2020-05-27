a40fb3d9ae00b8c8c42b745a4b0c4541c9b254f5
Patoliya-Divyesh <patoliyadivyesh.pd@gmail.com>
2020-05-27 19:08:57 +0530

    forms and user accounts

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
-from . import views
-from .views import BlogListView,BlogDetailView
+
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
+
 
 
 urlpatterns = [
-    path('hello/',views.index,name="blogindex"),
-    path('',BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/',BlogDetailView.as_view(), name='post_detail'),
+    # path('hello/',views.index,name="blogindex"),
+    path('post/<int:pk>/delete/',BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/',BlogUpdateView.as_view(), name='post_edit'), 
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
 
 ]
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
